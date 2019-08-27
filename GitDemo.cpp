ff15b6ad6615383198e8aa2e9b90aacee9aa065d
sachinmokashe <54577370+sachinmokashe@users.noreply.github.com>
2019-08-27 16:04:32 +0530

    Update GitDemo.cpp
    Added new function name as function()

--- GitDemo.cpp
+++ GitDemo.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+void function()
+{
+	
+	cout<<endl<<"Hello Function()";
+}
+
 int main()
 {
 	cout<<endl<<"Hello Git hub ";
+	function();
+	cout<<endl<<"Done...! ";
 	return 0;
 }
 
